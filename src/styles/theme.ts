ca9d102cfdc0ae7f65627c3ffe0647e3a5e98401
Renato Abreu <renatoabreukz@gmail.com>
2026-02-20 19:01:13 -0300

    chore: install zustand, install and configuration MUI (lib, icons and x-data-grid)

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  typography: {
+    h1: { fontSize: '1.75rem' },
+    h2: { fontSize: '1rem' },
+    body1: { fontSize: '0.875rem' },
+    body2: { fontSize: '0.75rem' },
+    subtitle1: { fontSize: '0.625rem' },
+  },
+  palette: {
+    primary: {
+      main: '#0b66ff',
+    },
+    secondary: {
+      main: '#e6f0ff',
+    },
+    text: {
+      primary: '#0F172A',
+      disabled: '#94A3B8',
+    },
+    warning: { main: '#F59E0B' },
+    success: { main: '#16A34A' },
+    error: { main: '#E02424' },
+    background: {
+      default: '#f7fafc',
+      paper: '#0B1220',
+    },
+    common: {
+      black: '#000',
+      white: '#fff',
+    },
+  },
+});
